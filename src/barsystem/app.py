0653e84c36d6e46904b3431034668a6f5a9c211a
siemdejong <28396796+siemdejong@users.noreply.github.com>
2024-10-13 17:00:41 +0200

    fix: add barsystem to path
    fixes #7

--- src/barsystem/app.py
+++ src/barsystem/app.py
@@ -16,7 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
+import sys
+from pathlib import Path
+
 import streamlit as st
+
+sys.path.append(str(Path(__file__).absolute().parent.parent))
 
 from barsystem.menu import menu
 
